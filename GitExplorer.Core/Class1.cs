3621b70e82373d69e593121d0994b75976c29763
Marc-Antoine Champagne <117603698+Marc-Antoine12@users.noreply.github.com>
2026-06-30 17:03:53 -0400

    feat: scaffold solution structure (#2)

---
+++ GitExplorer.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace GitExplorer.Core;
+
+public class Class1
+{
+
+}
---
+++ GitExplorer.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace GitExplorer.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
